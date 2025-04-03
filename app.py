84a2cd68335e8b88295c029e67c35fe36098a535
JeremQ27 <jeremiahquinto0627@gmail.com>
2025-04-03 11:49:21 +0800

    Implemented update, delete, mark and list functions. Added test file to ensure functions are working as intended.

--- app.py
+++ app.py
@@ -2,44 +2,28 @@ import json
 import sys, os
 from task_processes import process_command
 """
-    Steps for this project:
-     - Create a basic CLI structure to handle user inputs.
-
-     - Utilize a database that utilizes JSON file to save files
-        {
-            "id": int,
-            "description": str,
-            "status": todo, in-progress, done,
-            "createdAt": date
-            "updatedAt": date
-        }
-     -
-
-
-
-     Note:
-        Create a README file
+    main function, initial handling of tacker_db.json and main file handling
 """
 
 
-def create_json_file_it_not_exists():
-    filepath = 'tracker_db.json'
-    if not os.path.exists(filepath):
+def create_json_file_if_not_exists(fp):
+
+    if not os.path.exists(fp):
         try:
-            with open(filepath, 'w') as f:
+            with open(fp, 'w') as f:
                 data = {
                     "counter": 0,
                     "tasks": []
                 }
                 json.dump(data, f, indent=4)
-            print(f'File created: {filepath}')
+            print(f'File created: {fp}')
         except OSError as e:
             print(f"Error creating file: {e}")
 
 
 
 if __name__ == "__main__":
-    create_json_file_it_not_exists()
+    create_json_file_if_not_exists('tracker_db.json')
     if len(sys.argv) >= 1:
         command = sys.argv[1]
         if sys.argv[2:]:
--- task_processes.py
+++ task_processes.py
@@ -1,12 +1,15 @@
 import json
 from datetime import date
 
+
+
 """
-        task_format
+        task_database format
         {
             'counter': int
         }
-        {tasks:[
+        {
+            'tasks': [
         {
             "id": int,
             "description": str,
@@ -16,38 +19,100 @@ from datetime import date
         },
         ]
 """
-def load_tasks():
-    with open('tracker_db.json', 'r') as f:
+
+DB_PATH = 'tracker_db.json'
+
+
+def load_tasks(fp):
+    with open(fp, 'r') as f:
         return json.load(f)
 
-def save_tasks(data):
-    with open('tracker_db.json', 'w') as f:
+
+def save_tasks(fp, data):
+    with open(fp, 'w') as f:
         return json.dump(data, f, indent=4)
 
 
-def process_command(command, args):
-    if command == "add":
-        add_task(args)
-
-    elif command == 'update':
-        print('Update Command')
-        print(args)
-    elif command == 'delete':
-        print('Delete Command')
-        print(args)
-
-
-def add_task(args):
-    data = load_tasks()
-    data["counter"] += 1
+def add_task(database, args):
+    database["counter"] += 1
     new_task = {
-        "id": data["counter"],
+        "id": database["counter"],
         "description": args[0],
         "status": "todo",
         "createdAt": str(date.today()),
         "updatedAt": str(date.today())
     }
-    data["tasks"].append(new_task)
-    save_tasks(data)
+    database["tasks"].append(new_task)
+
+
+def update_task(database, args):
+    target_id = int(args[0])
+    updated_desc = args[1]
+    for task in database["tasks"]:
+        if task["id"] == target_id:
+            task["description"] = updated_desc
+            task["updatedAt"] = str(date.today())
+            break
+
+
+def delete_task(database, args):
+    found = False
+    target_id = int(args[0])
+    updated_tasks = []
+    for task in database["tasks"]:
+        if target_id == task['id']:
+            found = True
+        else:
+            updated_tasks.append(task)
+    database["tasks"] = updated_tasks
+    if not found:
+        print(f'ID {target_id} not found. Delete not possible')
+
+
+def mark_task(database, command, args):
+    found = False
+    command = command[5:]
+    target_id = int(args[0])
+    for task in database["tasks"]:
+        if task['id'] == target_id:
+            if command == 'done':
+                task['status'] = 'done'
+            elif command =='in-progress':
+                task['status'] = 'in-progress'
+            found = True
+    if not found:
+        print(f'ID {target_id} not found. Update not possible.')
+
+
+def list_tasks(database, args):
+    check_str = None
+    if args:
+        check_str = args[0]
+    for task in database['tasks']:
+        if check_str and task['status'] == check_str or check_str is None:
+            print(f"Task: {task['description']}\nStatus: {task['status']}\nCreation Date: {task['createdAt']}\nLast Update: {task['updatedAt']}")
+            print('\n\n')
+
+
+def process_command(command, args):
+    database = load_tasks(DB_PATH)
+
+    if command == "add":
+        add_task(database, args)
+
+    elif command == 'update':
+        update_task(database, args)
+
+    elif command == 'delete':
+        delete_task(database, args)
+
+    elif command == 'mark-in-progress' or command == 'mark-done':
+        mark_task(database, command, args)
+
+    elif command == 'list':
+        list_tasks(database, args)
+
+    save_tasks(DB_PATH, database)
+
 
 
